d6fe76418923094b7b68660b84dbf3fd867c968c
Maarten Spit <mspit@hotmail.com>
2019-11-10 16:27:15 +0100

    added date

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<? echo "hello world 2";?>
+<? 
+
+$date = date('m/d/Y h:i:s a', time());
+
+echo "<html><body>";
+echo "<h2>hello world </h2>";
+echo "Het is ".$date.", jeweettoch";
+echo "</body></html>";
+
+?>
